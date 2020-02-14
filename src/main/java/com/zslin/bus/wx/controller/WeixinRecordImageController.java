a38ff3728ddcd4e80590d18f047791ccd8ff44e6
想攀登的胖子 <oncwnuGq03LDHWDaVmcUPH7TEkzI@git.weixin.qq.com>
2020-02-14 16:08:18 +0800

    图片

---
+++ src/main/java/com/zslin/bus/wx/controller/WeixinRecordImageController.java
@@ -0,0 +1,73 @@
+package com.zslin.bus.wx.controller;
+
+import com.zslin.basic.repository.SimplePageBuilder;
+import com.zslin.basic.repository.SimpleSortBuilder;
+import com.zslin.basic.repository.SpecificationOperator;
+import com.zslin.basic.utils.ParamFilterUtil;
+import com.zslin.bus.basic.dao.IActivityRecordDao;
+import com.zslin.bus.basic.dao.IActivityRecordImageDao;
+import com.zslin.bus.basic.model.ActivityRecord;
+import com.zslin.bus.basic.model.ActivityRecordImage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping(value = "weixin/recordImage")
+@Controller
+public class WeixinRecordImageController {
+
+    @Autowired
+    private IActivityRecordDao activityRecordDao;
+
+    @Autowired
+    private IActivityRecordImageDao activityRecordImageDao;
+
+    private static final String TEMP_PREFIX = "weixin/recordImage/";
+
+    @GetMapping(value = {"index", "", "/"})
+    public String index(Model model, Integer page, Integer recordId, HttpServletRequest request) {
+        page = (page==null||page<=0)?0:page;
+        Page<ActivityRecordImage> datas = activityRecordImageDao.findAll(ParamFilterUtil.getInstance().buildSearch(model, request,
+                (recordId!=null&&recordId>0)?new SpecificationOperator("recordId", "eq", recordId):null),
+                SimplePageBuilder.generate(page, 20, SimpleSortBuilder.generateSort("id_d")));
+        List<List<ActivityRecordImage>> result = rebuildData(datas.getContent());
+        model.addAttribute("data1", result.get(0));
+        model.addAttribute("data2", result.get(1));
+        model.addAttribute("datas", datas);
+        model.addAttribute("current", 1);
+        return TEMP_PREFIX + "index";
+    }
+
+    /** 重新构建数据 */
+    private List<List<ActivityRecordImage>> rebuildData(List<ActivityRecordImage> list) {
+        List<ActivityRecordImage> list1 = new ArrayList<>();
+        List<ActivityRecordImage> list2 = new ArrayList<>();
+        int i=0;
+        for(ActivityRecordImage a : list) {
+            if((i++)%2==0) {
+                list2.add(a);
+            } else {list1.add(a);}
+        }
+        List<List<ActivityRecordImage>> result = new ArrayList<>();
+        result.add(list1);
+        result.add(list2);
+        return result;
+    }
+
+    @GetMapping(value = "listRecord")
+    public String listRecord(Model model, Integer page, HttpServletRequest request) {
+        page = (page==null||page<=0)?0:page;
+        Page<ActivityRecord> datas = activityRecordDao.findAll(ParamFilterUtil.getInstance().buildSearch(model, request),
+                SimplePageBuilder.generate(page, SimpleSortBuilder.generateSort("id_d")));
+        model.addAttribute("datas", datas);
+        model.addAttribute("current", 2);
+        return TEMP_PREFIX + "listRecord";
+    }
+}
