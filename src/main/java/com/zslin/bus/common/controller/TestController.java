172dbb44ba40f6d20dad4b189effcf00cc06d8f0
想攀登的胖子 <oncwnuGq03LDHWDaVmcUPH7TEkzI@git.weixin.qq.com>
2020-06-13 02:35:08 +0800

    ------------------

---
+++ src/main/java/com/zslin/bus/common/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.zslin.bus.common.controller;
+
+import com.zslin.test.ImageHandleTools;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "test")
+public class TestController {
+
+    @Autowired
+    private ImageHandleTools imageHandleTools;
+
+    @GetMapping(value = "processImage")
+    public @ResponseBody
+    String processImage(Integer w) throws Exception {
+        imageHandleTools.process(w);
+        return "----------";
+    }
+}
