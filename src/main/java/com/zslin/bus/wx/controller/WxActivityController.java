9927ffc34793cf08290553f230c8fd58dd306054
想攀登的胖子 <oncwnuGq03LDHWDaVmcUPH7TEkzI@git.weixin.qq.com>
2020-06-16 09:29:15 +0800

    1111111111

--- src/main/java/com/zslin/bus/wx/controller/WxActivityController.java
+++ src/main/java/com/zslin/bus/wx/controller/WxActivityController.java
@@ -4,13 +4,11 @@ import com.zslin.basic.repository.SimplePageBuilder;
 import com.zslin.basic.repository.SimpleSortBuilder;
 import com.zslin.basic.repository.SpecificationOperator;
 import com.zslin.basic.utils.ParamFilterUtil;
-import com.zslin.bus.basic.dao.IActivityCommentDao;
-import com.zslin.bus.basic.dao.IActivityDao;
-import com.zslin.bus.basic.dao.IActivityRecordDao;
-import com.zslin.bus.basic.dao.IActivityStudentDao;
+import com.zslin.bus.basic.dao.*;
 import com.zslin.bus.basic.model.Activity;
 import com.zslin.bus.basic.model.ActivityComment;
 import com.zslin.bus.basic.model.ActivityRecord;
+import com.zslin.bus.basic.model.Department;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -40,6 +38,9 @@ public class WxActivityController {
     @Autowired
     private IActivityStudentDao activityStudentDao;
 
+    @Autowired
+    private IDepartmentDao departmentDao;
+
     /** 获取当前活动 */
     @GetMapping(value = "current")
     public String current(Model model, HttpServletRequest request) {
@@ -58,9 +59,17 @@ public class WxActivityController {
     }
 
     @GetMapping(value = "index")
-    public String index(Model model, Integer page, HttpServletRequest request) {
-        Page<Activity> datas = activityDao.findAll(ParamFilterUtil.getInstance().buildSearch(model, request, new SpecificationOperator("status", "eq", "1")),
+    public String index(Model model, Integer depId, Integer page, HttpServletRequest request) {
+        Page<Activity> datas = activityDao.findAll(ParamFilterUtil.getInstance().buildSearch(model, request,
+                new SpecificationOperator("status", "eq", "1"),
+                (depId==null || depId<=0)?null:new SpecificationOperator("depId", "eq", depId)),
                 SimplePageBuilder.generate(page, SimpleSortBuilder.generateSort("createDate_d")));
+        if(depId!=null && depId>0) {
+            Department dep = departmentDao.findOne(depId);
+            model.addAttribute("department", dep);
+        } else {
+            model.addAttribute("department", null);
+        }
         model.addAttribute("datas", datas);
         return "weixin/activity/index";
     }
